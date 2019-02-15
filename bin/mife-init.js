068df632b4e259b15cfeb1c589a2c8892641f85e
lsqaisen <lai413633@outlook.com>
2019-02-15 15:36:54 +0800

    package.json module

--- bin/mife-init.js
+++ bin/mife-init.js
@@ -11,24 +11,29 @@ const logSymbols = require('log-symbols');
 const download = require('../lib/download');
 const generator = require('../lib/generator');
 
-program.usage('<project-name>').parse(process.argv)
-// 根据输入，获取项目名称
-let projectName = program.args[0]
-if (!projectName) {  // project-name 必填
-  // 相当于执行命令的--help选项，显示help信息，这是commander内置的一个命令选项
+program.usage('<project-name>')
+  .option('-r, --repository [repository]', 'assign to repository')
+  .parse(process.argv);
+
+let projectName = program.args[0];
+
+if (!projectName) {
+  // Project-name is empty, showing --help(project-name为空，显示--help)
   program.help()
   return
 }
 
-const list = glob.sync('*')
+const list = glob.sync('*');
+let rootName = path.basename(process.cwd());
 let next = undefined
+
 if (list.length) {
   if (list.filter(name => {
     const fileName = path.resolve(process.cwd(), path.join('.', name))
     const isDir = fs.lstatSync(fileName).isDirectory();
     return name.indexOf(projectName) !== -1 && isDir
   }).length !== 0) {
-    console.log(`项目${projectName}已经存在`)
+    console.log(`${projectName} directory is exist`)
     return
   }
   next = Promise.resolve(projectName)
@@ -36,65 +41,117 @@ if (list.length) {
   next = inquirer.prompt([
     {
       name: 'buildInCurrent',
-      message: '当前目录为空，且目录名称和项目名称相同，是否直接在当前目录下创建新项目？',
+      message: 'The current directory is empty and the directory name is the same as the project name. Do you want to create a new project directly in the current directory?',
+      // message: '当前目录为空，目录名称和项目名称相同，是否直接在当前目录下创建新项目？',
       type: 'confirm',
       default: true
     }
   ]).then(answer => {
-    return Promise.resolve(answer.buildInCurrent ? '.' : projectName)
+    return Promise.resolve(answer.buildInCurrent ? projectName : '.')
   })
 } else {
   next = Promise.resolve(projectName)
 }
 
-next && go()
+go()
 
 function go() {
-  next.then(projectRoot => {
-    if (projectRoot !== '.') {
-      fs.mkdirSync(projectRoot)
-    }
-    return download(projectRoot).then(target => {
-      return {
-        name: projectRoot,
-        root: projectRoot,
-        downloadTemp: target
-      }
-    })
-  }).then(context => {
-    return inquirer.prompt([
-      {
-        name: 'projectName',
-        message: '项目的名称',
-        default: context.name
-      }, {
-        name: 'projectVersion',
-        message: '项目的版本号',
-        default: '1.0.0'
-      }, {
-        name: 'projectDescription',
-        message: '项目的简介',
-        default: `A project named ${context.name}`
-      }
-    ]).then(answers => {
-      return latestVersion('macaw-ui').then(version => {
-        answers.supportUiVersion = version
-        return {
-          ...context,
-          metadata: {
-            ...answers
+  next.then(projectName => {
+    if (projectName !== '.') {
+      fs.mkdirSync(projectName)
+      const url = program.repository ? program.repository : 'lsqaisen/mife-demo.git#master'
+      download(url, projectName)
+        .then(target => {
+          return {
+            name: projectName,
+            root: projectName,
+            downloadTemp: target
           }
-        }
-      }).catch(err => {
-        return Promise.reject(err)
-      })
-    })
-    return generator(context)
-  }).then(context => {
-    console.log(logSymbols.success, chalk.green('创建成功:)'))
-    console.log()
-    console.log(chalk.green('cd ' + context.root + '\nnpm install\nnpm run dev'))
-  }).catch(err => {
-    console.error(logSymbols.error, chalk.red(`创建失败：${err.message}`))
+        })
+        .then(context => {
+          console.log(context)
+          return inquirer.prompt([
+            {
+              name: 'projectName',
+              message: 'name',
+              default: context.name
+            },
+            {
+              name: 'projectVersion',
+              message: 'version',
+              default: '1.0.0'
+            },
+            {
+              name: 'projectDescription',
+              message: 'description',
+              default: `A project named ${context.name}`
+            }
+          ]).then(answers => {
+            return {
+              ...context,
+              metadata: {
+                ...answers
+              }
+            }
+          })
+        })
+        .then(context => {
+          return generator(context.metadata, context.downloadTemp, path.parse(context.downloadTemp).dir);
+        })
+        .then(res => {
+          console.log(logSymbols.success, chalk.green('found success:)'))
+          console.log(chalk.green('cd ' + res.dest + '\nnpm install\nnpm run dev'))
+        })
+        .catch(error => {
+          console.error(logSymbols.error, chalk.red(`found faild：${error.message}`))
+        })
+    }
   })
+  // next.then(projectRoot => {
+  //   if (projectRoot !== '.') {
+  //     fs.mkdirSync(projectRoot)
+  //   }
+  //   return download(projectRoot).then(target => {
+  //     return {
+  //       name: projectRoot,
+  //       root: projectRoot,
+  //       downloadTemp: target
+  //     }
+  //   })
+  // }).then(context => {
+  //   return inquirer.prompt([
+  //     {
+  //       name: 'projectName',
+  //       message: '项目的名称',
+  //       default: context.name
+  //     }, {
+  //       name: 'projectVersion',
+  //       message: '项目的版本号',
+  //       default: '1.0.0'
+  //     }, {
+  //       name: 'projectDescription',
+  //       message: '项目的简介',
+  //       default: `A project named ${context.name}`
+  //     }
+  //   ]).then(answers => {
+  //     return latestVersion('macaw-ui').then(version => {
+  //       answers.supportUiVersion = version
+  //       return {
+  //         ...context,
+  //         metadata: {
+  //           ...answers
+  //         }
+  //       }
+  //     }).catch(err => {
+  //       return Promise.reject(err)
+  //     })
+  //   })
+  //   return generator(context)
+  // }).then(context => {
+  //   console.log(logSymbols.success, chalk.green('创建成功:)'))
+  //   console.log()
+  //   console.log(chalk.green('cd ' + context.root + '\nnpm install\nnpm run dev'))
+  // }).catch(err => {
+  //   console.error(logSymbols.error, chalk.red(`创建失败：${err.message}`))
+  // })
 }
--- bin/mife.js
+++ bin/mife.js
@@ -1,6 +1,14 @@
-const program = require('commander')
+#!/usr/bin/env node
 
+const program = require('commander')
 program.version('1.0.0')
-  .usage('<command> [项目名称]')
-  .command('init', '创建新项目')
+  .usage('<command> [project-name]')
+  .command('hello', 'this will run mife-hello.js') // Git-style sub-commands
+  .command('test', 'this will run mife-test.js') // Git-style sub-commands
+  .command('init', 'this will run mife-init.js')
   .parse(process.argv)
+
+// test case
+// node ./bin/mife.js hello
+// node ./bin/mife.js test my-project
+// node ./bin/mife.js init my-project
