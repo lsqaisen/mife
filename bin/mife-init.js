615977b7adffa05f136b7e6635c38ede64a1b272
lsqaisen <lai413633@outlook.com>
2019-02-15 11:39:49 +0800

    add generator

--- bin/mife-init.js
+++ bin/mife-init.js
@@ -4,48 +4,97 @@ const program = require('commander');
 const path = require('path');
 const fs = require('fs');
 const glob = require('glob');
+const inquirer = require('inquirer');
+const latestVersion = require('latest-version');
+const chalk = require('chalk');
+const logSymbols = require('log-symbols');
 const download = require('../lib/download');
+const generator = require('../lib/generator');
 
 program.usage('<project-name>').parse(process.argv)
-
 // 根据输入，获取项目名称
 let projectName = program.args[0]
-
 if (!projectName) {  // project-name 必填
   // 相当于执行命令的--help选项，显示help信息，这是commander内置的一个命令选项
   program.help()
   return
 }
 
-const list = glob.sync('*')  // 遍历当前目录
-let rootName = path.basename(process.cwd())
-if (list.length) {  // 如果当前目录不为空
-  const hasProject = false;
-  list.filter(async name => {
+const list = glob.sync('*')
+let next = undefined
+if (list.length) {
+  if (list.filter(name => {
     const fileName = path.resolve(process.cwd(), path.join('.', name))
-    const isDir = await new Promise((resolve) => fs.stat(fileName, (_, stats) => {
-      resolve(stats.isDirectory())
-    }))
-    console.log(name, isDir, name.indexOf(projectName) !== -1 && isDir)
-    hasProject = name.indexOf(projectName) !== -1 && isDir;
+    const isDir = fs.lstatSync(fileName).isDirectory();
     return name.indexOf(projectName) !== -1 && isDir
-  })
-  console.log(hasProject)
-  if (hasProject) {
+  }).length !== 0) {
     console.log(`项目${projectName}已经存在`)
     return
   }
-  rootName = projectName
+  next = Promise.resolve(projectName)
 } else if (rootName === projectName) {
-  rootName = '.'
+  next = inquirer.prompt([
+    {
+      name: 'buildInCurrent',
+      message: '当前目录为空，且目录名称和项目名称相同，是否直接在当前目录下创建新项目？',
+      type: 'confirm',
+      default: true
+    }
+  ]).then(answer => {
+    return Promise.resolve(answer.buildInCurrent ? '.' : projectName)
+  })
 } else {
-  rootName = projectName
+  next = Promise.resolve(projectName)
 }
 
-go()
+next && go()
 
 function go() {
-  download(projectName)
-    .then(target => console.log(target))
-    .catch(err => console.log(err))
+  next.then(projectRoot => {
+    if (projectRoot !== '.') {
+      fs.mkdirSync(projectRoot)
+    }
+    return download(projectRoot).then(target => {
+      return {
+        name: projectRoot,
+        root: projectRoot,
+        downloadTemp: target
+      }
+    })
+  }).then(context => {
+    return inquirer.prompt([
+      {
+        name: 'projectName',
+        message: '项目的名称',
+        default: context.name
+      }, {
+        name: 'projectVersion',
+        message: '项目的版本号',
+        default: '1.0.0'
+      }, {
+        name: 'projectDescription',
+        message: '项目的简介',
+        default: `A project named ${context.name}`
+      }
+    ]).then(answers => {
+      return latestVersion('macaw-ui').then(version => {
+        answers.supportUiVersion = version
+        return {
+          ...context,
+          metadata: {
+            ...answers
+          }
+        }
+      }).catch(err => {
+        return Promise.reject(err)
+      })
+    })
+    return generator(context)
+  }).then(context => {
+    console.log(logSymbols.success, chalk.green('创建成功:)'))
+    console.log()
+    console.log(chalk.green('cd ' + context.root + '\nnpm install\nnpm run dev'))
+  }).catch(err => {
+    console.error(logSymbols.error, chalk.red(`创建失败：${err.message}`))
+  })
 }
